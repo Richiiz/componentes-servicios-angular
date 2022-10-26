d8e75f89292d4ba3e00dd1e5667886bb183eeed5
Richiiz <ricardo.rodriguez@librerialeo.com.mx>
2022-10-26 11:53:12 -0500

    usando inputs y ouputs para llamar acciones de padre a hijo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'store';
+// clase puesta en la class binding
+widthImg = 10
+
+
+  imgParent = '';
+
+  onLoaded(img: string){
+    console.log('log Padre', img);
+
+  }
 }
