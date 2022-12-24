be6d5727ae2b8cafb4afe70e1ecfafe220ae06d8
Richiiz <ricardo.rodriguez@librerialeo.com.mx>
2022-12-24 12:40:22 -0600

    Manejo de headers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 
 // aqui volvemos a importar nuestro tipado especifico para no tener que reescribir codigo
-import {AuthService} from './services/auth.service'
-import {UsersService} from './services/users.service'
+import { UsersService } from './services/users.service';
 
 
 @Component({
@@ -16,8 +15,7 @@ showImg = true;
 token = '';
 
 constructor(
-  private authService: AuthService,
-  private UsersService: UsersService,
+  private UsersService: UsersService
 ) {
 
 }
@@ -29,8 +27,6 @@ widthImg = 10
 // aqui añadimos el tipado que se importo, recuerda que es buena practica tipar :D.
 // el tipado nos ayuda a verificar que todo este  correcto,
 // recuerda tambien que los arrays deben tener los mismos elementos del tipado, si no, indicara error.
-
-  imgParent = '';
 
   onLoaded(img: string){
     console.log('log Padre', img);
@@ -50,13 +46,6 @@ widthImg = 10
     })
     .subscribe(rta => {
       console.log(rta);
-    });
-  }
-
-login(){
-    this.authService.login('richi@gmail.com', '1212')
-    .subscribe(rta => {
-      this.token = rta.access_token;
     });
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
 
 import { environment } from './../../environments/environment';
 import { Auth } from './../models/auth.model';
@@ -20,7 +20,14 @@ private apiUrl = `${environment.API_URL}/api/auth`;
   return this.http.post<Auth>(`${this.apiUrl}/login`, {email, password});
     }
 
-    profile(){
-  return this.http.get(`${this.apiUrl}/profile`);
-    }
+    profile(token: string){
+      // const headers = new HttpHeaders();
+      // headers.set('Authorization',`Bearer ${token}`);
+      return this.http.get<User>(`${this.apiUrl}/profile`, {
+        headers: {
+        Authorization: `Bearer ${token}`,
+        // 'Content-type': 'application/json'
+      }
+    });
+  }
 }
